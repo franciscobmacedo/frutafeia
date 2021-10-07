3c4d7862b68d6b6253cd5a0365847b59d9cadde6
Francisco Macedo <franciscovcbm@gmail.com>
2021-10-07 18:27:24 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,12 @@ rm -r static
 mkdir static
 cd frontend && yarn build
 
-# push it and delete branch
+# add to tracking
 git add  ../.
 git add -f ../static
 git commit -m "deploy"
+
+# push it and delete branch
 git push origin deploy
 git checkout main
 git branch -D deploy
